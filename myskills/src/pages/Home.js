f13d83ef25592c68b6394b7c7cc8cd688e54a846
Pedro H. C. Carrasco <pedrocamussi@gmail.com>
2022-04-24 21:51:03 -0300

    using stylesheet inline
    just to know how to do it

--- myskills/src/pages/Home.js
+++ myskills/src/pages/Home.js
@@ -1,12 +1,56 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Platform, TouchableOpacity } from 'react-native';
 
 export function Home() {
   return (
-    <>
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>React Native</Text>
-      </View>
-    </>
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome, Pedro</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="New Skill"
+        placeholderTextColor="#555"
+      />
+      <TouchableOpacity 
+        style={styles.button}
+        activeOpacity={.7}>
+          <Text style={styles.buttonText}>Add</Text>
+      </TouchableOpacity>
+      <Text style={[styles.title, {marginTop: 20 }]}>
+        My Skills
+      </Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#121015",
+    paddingVertical: 70,
+    paddingHorizontal: 30,
+  },
+  title: {
+    color: "#fff",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  input: {
+    backgroundColor: "#1f1e25",
+    color: "#fff",
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 7,
+  },
+  button:{
+    backgroundColor:'#A370F7',
+    padding:15,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 20
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight:'bold'
+  }
+});
