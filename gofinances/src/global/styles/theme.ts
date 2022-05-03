4064ac3e8e3daebfa53fc1a43884eff182f34778
Pedro H. C. Carrasco <pedrocamussi@gmail.com>
2022-05-03 00:09:43 -0300

    feature: adding and styling the icon

--- gofinances/src/global/styles/theme.ts
+++ gofinances/src/global/styles/theme.ts
@@ -1,8 +1,8 @@
 export default {
     colors:{
         primary: '#5636D3',
-        secundary: '#FF872C',
-        secundary_light: 'rgba(255,135,44,0.3)',
+        secondary: '#FF872C',
+        secondary_light: 'rgba(255,135,44,0.3)',
 
         success: '#12A454',
         success_light:  'rgba(18,164,84,0.5)',
--- gofinances/src/screens/Dashboard/index.tsx
+++ gofinances/src/screens/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import {
     Container,
     Header,
@@ -7,7 +8,8 @@ import {
     Photo,
     User,
     UserGreeting,
-    UserName
+    UserName,
+    Icon
 } from './styles';
 
 export function Dashboard(){
@@ -22,6 +24,7 @@ export function Dashboard(){
                             <UserName>Pedro</UserName>
                         </User>
                     </UserInfo>
+                    <Icon name="power"/>
                 </UserWrapper>
             </Header>
         </Container>
