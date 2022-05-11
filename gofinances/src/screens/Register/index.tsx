730826e810271b0f11c4b9da1da68e7d61349e72
Pedro H. C. Carrasco <pedrocamussi@gmail.com>
2022-05-10 22:37:20 -0300

    feat: category select component

--- gofinances/src/screens/Register/index.tsx
+++ gofinances/src/screens/Register/index.tsx
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import { Input } from "../../components/Form/Input";
 import { Button } from "../../components/Form/Button";
 import { TransactionTypeButton } from "../../components/Form/TransactionTypeButton";
+import { CategorySelect } from "../../components/Form/CategorySelect";
 
 import { Container, Header, Title, Form, Fields, TransactionsTypes } from "./styles";
 
@@ -26,7 +27,7 @@ export function Register(){
                         <TransactionTypeButton type="up" title="Income" onPress={() => handleTransactionTypeSelect('up')} isActive={transactionType === 'up'}/>
                         <TransactionTypeButton type="down" title="Outcome" onPress={() => handleTransactionTypeSelect('down')} isActive={transactionType=== 'down'}/>
                     </TransactionsTypes>
-
+                    <CategorySelect title={'Categoria'}/>
                 </Fields>
                 <Button title="Enviar"/>
             </Form>
