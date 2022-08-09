5cfa65064b5e8828d388d983e26876206d96a04f
Pedro H. C. Carrasco <pedrocamussi@gmail.com>
2022-08-08 23:27:58 -0300

    feat: scheduling details screen

--- rentx/App.tsx
+++ rentx/App.tsx
@@ -9,6 +9,7 @@ import { Home } from './src/screens/Home';
 import { CarDetails } from './src/screens/CarDetails';
 import theme from './src/styles/theme';
 import { Scheduling } from './src/screens/Scheduling';
+import { SchedulingDetails } from './src/screens/SchedulingDetails';
 
 
 export default function App() {
@@ -34,7 +35,7 @@ export default function App() {
   
   return (
     <ThemeProvider theme={theme}>
-      <Scheduling/>
+      <SchedulingDetails/>
     </ThemeProvider>
 
   );
