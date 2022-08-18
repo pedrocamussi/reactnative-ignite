43a8daf6ec2af66c4695f0105042c447b641f4c0
Pedro H. C. Carrasco <pedrocamussi@gmail.com>
2022-08-17 23:47:45 -0300

    feat: car icon by kind of data

--- rentx/src/components/Car/index.tsx
+++ rentx/src/components/Car/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { RectButtonProps } from 'react-native-gesture-handler';
 
 import GasolineSvg from '../../assets/gasoline.svg'
+import { CarDTO } from '../../dtos/CarDTO';
+import { getAccessoryIcon } from '../../utils/getAccessoryIcon';
 
 import {
   Container,
@@ -16,22 +18,14 @@ import {
   CarImage
 } from './styles';
 
-interface CarData {
-    brand: string;
-    name: string;
-    rent: {
-        period: string;
-        price: number;
-    },
-    thumbnail: string;
-}
 
 interface Props extends RectButtonProps{
-    data: CarData;
+    data: CarDTO;
 }
 
 export function Car({data, ...rest} : Props){
-  return(
+    const MotorIcon = getAccessoryIcon(data.fuel_type)
+    return(
     <Container {...rest}>
         <Details>
             <Brand>{data.brand}</Brand>
@@ -42,7 +36,7 @@ export function Car({data, ...rest} : Props){
                     <Price>{`R$${data.rent.price}`}</Price>
                 </Rent>
                 <Type>
-                    <GasolineSvg/>
+                    <MotorIcon/>
                 </Type>
             </About>
         </Details>
